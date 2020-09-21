1079d46e018fb3fd418de102b34af4b9b1a84dc9
Owenthecoder1 <71456832+Owenthecoder1@users.noreply.github.com>
2020-09-21 10:07:41 -0700

    finished it

--- Function.js
+++ Function.js
@@ -1,3 +1,11 @@
-function wassup(){
-	alert("wassup")
+function color_change(){
+	var color = prompt("What color?")
+	if(color!="" && color!=null){
+		this.style.backgroundColor=color
+    }
+    if(color == "white"){
+    	document.body.style.backgroundColor = "black"
+    }else{
+    	document.body.style.backgroundColor = "white"	
+    }
 }
--- main.js
+++ main.js
@@ -1,1 +1,2 @@
-wassup()
+var mr_square = document.getElementById("mr_square")
+mr_square.addEventListener("click",color_change)
